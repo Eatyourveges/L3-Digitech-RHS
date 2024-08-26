3afe7994a0178cfb70b07ee0ee50529b2ec2bea5
Eatyourveges <bobbykurt220@gmail.com>
2024-08-27 09:02:12 +1200

    Add files via upload
    This code has me starting to create the display for how much time is left for play time just needing to figure out how to get it working again.

--- Whack-The-Mole.py
+++ Whack-The-Mole.py
@@ -1,33 +1,42 @@
-# * imports all functions from that library
+# Whack The Mole 
+# Imports all functions from that library
 import sys
+import random
+import time
 from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QInputDialog, QMessageBox, QLabel, QVBoxLayout, QHBoxLayout
 from PyQt5.QtCore import QTimer
-import random 
 
-
-#This class is to define the entire process of the game
-class ButtonGrid(QWidget):
-    #This function has all the variables in it and initializes the code
+# This class is to define the entire process of the game
+class WhackAMoleGame(QWidget):
     def __init__(self):
         super().__init__()
+
         # Game settings
         self.num_holes = 25
         self.mole_hole = None
         self.score = 0
-        self.game_ended = False
-        self.init_ui()
+        self.timer_left = 0
+        self.game_running = True
+        
+        self.setWindowTitle('Whack-The-Mole')
 
         # Layouts
-        self.main_layout = QVBoxLayout
-        self.grid_layout = QHBoxLayout
-
+        self.main_layout = QVBoxLayout()
+        self.grid_layout = QHBoxLayout()
+    
+        # Score and Time display  
+        self.timer_left = self.timer_limit
         self.score_label = QLabel(f'Score: {self.score}')
+        self.timer_label = QLabel(f"Time Left: {self.timer_left} seconds",self)
+        
         self.main_layout.addWidget(self.score_label)
+        self.main_layout.addWidget(self.timer_label)
 
         # Buttons for holes
         self.buttons = [QPushButton('') for _ in range(self.num_holes)]
         self.buttons_layout = QVBoxLayout()
         
+        # Creates the grid layout
         for i in range(5):
             row_layout = QHBoxLayout()
             for j in range(5):
@@ -40,76 +49,55 @@ class ButtonGrid(QWidget):
         self.main_layout.addLayout(self.buttons_layout)
         self.setLayout(self.main_layout)
 
-
-    #This function creates the board 
-    def init_ui(self):
-        self.setWindowTitle('Wack-The-Moe')
-
-        #Sets the time limit for the game 
-
-        self.time_limit, ok = QInputDialog.getInt(self, 'Wack-the-Moe Timer', f'Enter the game time in seconds between 30sec to 60sec:', min=30, max=60)
-
-
-        self.buttons = [QPushButton('') for _ in range(self.num_holes)]
-        self.buttons_layout = ()
-
-        self.grid = QGridLayout()
-        self.setLayout(self.grid)
-        self.buttons = [[QPushButton(' ') for _ in range(5)] for _ in range(5)]
-
-        self.buttons = [QPushButton('') for _ in range(self.num_holes)]
-        self.buttons_layout = QVBoxLayout()
+        # Timer for the player
+        self.timer_limit = QTimer()
+        self.timer_limit.timeout.connect(self.timer_limit)
+        self.timer_limit, ok = QInputDialog.getInt(self, 'Whack-the-Mole Timer', f'Enter the game time in seconds between 15 sec to 60 sec:', min=15, max=60)
+        QTimer.singleShot(self.timer_limit * 1000, self.end_game)
         
-        for i in range(3):
-            row_layout = QHBoxLayout()
-            for j in range(3):
-                button = self.buttons[i*3 + j]
-                button.setFixedSize(100, 100)
-                button.clicked.connect(self.on_button_click)
-                row_layout.addWidget(button)
-            self.buttons_layout.addLayout(row_layout)
+        # Timer for the moles to spawn
+        self.show_mole_timer = QTimer()
+        self.show_mole_timer.timeout.connect(self.show_mole)
+        self.show_mole_timer.start(1000)
 
-        self.main_layout.addLayout(self.buttons_layout)
-        self.setLayout(self.main_layout)
-
-        for row in range(self.number_of_rows):
-            for col in range(self.number_of_cols):
-                button = self.buttons[row][col]
-                button.setFixedSize(100,100)
-                button.clicked.connect(lambda ch, row=row, col=col: self.button_clicked(row, col))
-                self.grid.addWidget(button, row, col)
-
+    # Defines what the mole looks like 
     def show_mole(self):
         if self.mole_hole is not None:
             self.buttons[self.mole_hole].setText('')
         
+        # Creates what hole the mole is located 
         self.mole_hole = random.randint(0, self.num_holes - 1)
         self.buttons[self.mole_hole].setText('ʕ•ᴥ•ʔ')
 
+    # Defines what happens when the mole is cilcked 
     def on_button_click(self):
         button = self.sender()
         if button.text() == 'ʕ•ᴥ•ʔ':
             self.score += 1
             self.score_label.setText(f'Score: {self.score}')
             button.setText('')
+            self.mole_hole = random.randint(0, self.num_holes - 1)
+            self.buttons[self.mole_hole].setText('')
             self.mole_hole = None
 
+    def update_timer(self):
+        self.timer_left -= 1 
+        self.timer_label.setText(f'Time Left: {self.timer_left} sec')
+        if self.timer_left == 0:
+            self.end_game
+
+    # Defines what happens when the timer is over
     def end_game(self):
         QMessageBox.information(self, 'Game Over', f'Game over! Final score: {self.score}')
-        print('Game Over')
-        file = open('file.txt', 'w')
-        file.write(f'You had {self.score} turns')
+        print ('Game Over!')
         self.close()
+        file = open('Whack The Mole Score', 'w')
+        file.write(f'You scored: {self.score}')
+        file.close()
 
-
-# Makes sure it is running the Main code
+# Closes the game
 if __name__ == '__main__':
-
-     # Create an instance of QApplication
     app = QApplication(sys.argv)
-
-    # Create a main application window (QWidget)
-    game = ButtonGrid()
+    game = WhackAMoleGame()
     game.show()
-    # Execute the application's event loop
     sys.exit(app.exec_())
